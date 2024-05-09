5d26986f5d067440bef7c823cbd0b4b549d89215
tanzizhen <923360732@qq.com>
2024-05-09 16:45:50 +0800

    init

---
+++ src/server/Application.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2024/5/9
+ * Time: 16:43
+ */
+
+namespace Wm\Wmsoft\server;
+
+
+use Wm\Wmsoft\wm\Wm;
+
+class Application
+{
+    /**
+     * object container
+     * @var array
+     */
+    static public $container = array();
+
+    /**
+     * Application constructor.
+     * @param array $config
+     */
+    public function __construct($config = array())
+    {
+        foreach ($config as $key => $value) {
+            Wm::$app->$key = $value;
+
+            if ($key == 'time_zone') {
+                $this->setTimeZone($value);
+            }
+        }
+    }
+
+    /**
+     * 设置时区
+     * @param $value
+     */
+    public function setTimeZone($value)
+    {
+        date_default_timezone_set($value);
+    }
+
+    /**
+     * init
+     */
+    public function init()
+    {
+
+    }
+
+
+    /**
+     * run
+     */
+    public function run()
+    {
+
+    }
+
+}
---
+++ src/wm/Wm.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2024/5/9
+ * Time: 16:42
+ */
+
+namespace Wm\Wmsoft\wm;
+
+
+class Wm
+{
+    public static $app;
+
+    public function __construct()
+    {
+        if (!self::$app) {
+            self::$app = $this;
+        }
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+
+        // TODO: Implement __get() method.
+        if (isset(self::$app->components) && isset(self::$app->components[$name]) && !isset(Wm::$app->$name)) {
+            self::$app->$name = $this->create_object(self::$app->components[$name]);
+
+            return self::$app->$name;
+        }
+    }
+
+
+    /**
+     * Creating object method
+     * @param $arr
+     * @return mixed
+     */
+    public function create_object($arr)
+    {
+        $class = $arr['class'];
+        return new $class($arr);
+    }
+
+}
+
+new Wm();
