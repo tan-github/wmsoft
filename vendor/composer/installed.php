b04a8d3c17dae05d577233ecd1d22ca06bb25b2f
tanzizhen <923360732@qq.com>
2024-05-09 16:35:02 +0800

    初始化

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'twm/wmsoft',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5ce75183f7d2f2ed67d60f31278283861da87f94',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'twm/wmsoft' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5ce75183f7d2f2ed67d60f31278283861da87f94',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
